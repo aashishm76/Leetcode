201585913cdfc22eb7918babdab76df2bbed9345
Aashish <aashishm97@knights.ucf.edu>
2018-08-06 18:23:26 -0400

    709. To Lower Case

---
+++ ToLowerCase.java
@@ -0,0 +1,11 @@
+//LeetCode 709: To Lower Case
+
+public class Solution
+{
+  public String toLowerCase(String str)
+  {
+    String loweredString = new String(str.toLowerCase());
+
+    return loweredString;
+  }
+}
